e38b8d820a897e1b1b6a856e87fcc2e2949b7ac2
kevinhermawan009 <reactjugger@gmail.com>
2022-11-06 21:29:30 +0700

    Creating Home Page

---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Home() {
+  return (
+    <div className='box'>
+	<div className='wave -one'></div>
+	<div className='wave -two'></div>
+	<div className='wave -three'></div>
+</div>
+  )
+}
