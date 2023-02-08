dfc73f64c7ce56e1ace320c47d6a629d6e8ced2a
kevinhermawan009 <reactjugger@gmail.com>
2023-02-08 22:57:22 +0700

    implement outlet for parent sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Home } from "./views/Home";
-import React from "react";
-
+import { Outlet } from 'react-router-dom';
+import { Dashboard } from "./views/Dashboard";
+import { Sidebar } from "./components/sideMenu/Sidebar";
+import React, { useEffect, useState } from "react";
+import Dummy from "./views/Dummy";
+const SidebarLayout = () => (
+  <>
+    <Sidebar />
+    <Outlet />
+  </>
+);
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    lat: null,
-    long: null
-  };
-}
-render() {
-  // GELOCATION==========
-  window.navigator.geolocation.getCurrentPosition(
-    position => this.setState({
-      lat: position.coords.accuracy,
-      long: position.coords.longitude
-    }),
-    err => console.log('Err', err)
-  );
-  return (
-    <main className='App'>
-      <Routes className='App'>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </main >
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      long: null,
+      auth: true,
+    };
+  }
+  
+  render() {
+    // GELOCATION==========
+    // window.navigator.geolocation.getCurrentPosition(
+    //   (position) =>
+    //     this.setState({
+    //       lat: position.coords.accuracy,
+    //       long: position.coords.longitude,
+    //     }),
+    //   (err) => console.log("Err", err)
+    // );
 
-
-  );
+    return (
+      <BrowserRouter>
+        {/* {this.state.auth === true ? <Sidebar /> : null} */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route  element={<Dashboard />} />
+          <Route path="/Sidebar/Dashboard/" element={<SidebarLayout/>}>
+            {/* <Route index element={<Home/>} /> */}
+            <Route path="/Sidebar/Dashboard/Dummy" element={<Dummy/>} />
+            <Route path="/Sidebar/Dashboard/DummyShit" element={<Dummy/>} />
+          </Route>
+          {/* <Route path="/Sidebar/Dashboard/Dummy" element={<Dummy/>} /> */}
+    
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-};
 export default App;
